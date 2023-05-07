4685542952fb3f4b8df00c788b0dcca71cb5bc66
AgaToma <>
2023-05-07 12:38:28 +0000

    Fix: project name typo

--- lighthousebnb/asgi.py
+++ lighthousebnb/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for lighhousebnb project.
+ASGI config for lighthousebnb project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lighhousebnb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lighthousebnb.settings')
 
 application = get_asgi_application()
--- lighthousebnb/wsgi.py
+++ lighthousebnb/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lighhousebnb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lighthousebnb.settings')
 
 application = get_wsgi_application()
