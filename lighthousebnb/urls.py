4589173d82427a5024128fc7bf67571c2e48f3e2
AgaToma <>
2023-05-14 17:38:30 +0000

    Add allauth templates

--- lighthousebnb/urls.py
+++ lighthousebnb/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from allauth.account.views import signup, login, logout
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('admin/', admin.site.urls)
+    path('accounts/', include("allauth.urls")),
 ]
