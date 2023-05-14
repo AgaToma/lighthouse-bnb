59b5d7adb49fb6c0295c73083a18e86d6ab7472e
AgaToma <>
2023-05-14 11:23:09 +0000

    Add custom user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "is_admin")
+    list_filter = ("email", "is_admin")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_admin",)}),
+        (
+            "Advanced options",
+            {
+                "classes": ["collapse"],
+                "fields": ["is_active", "date_joined"],
+            },
+        ),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2")
+        }
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+    
