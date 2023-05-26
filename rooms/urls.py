a4e99821aa1ea9eeb68b3b5107d71e773eac68a4
AgaToma <>
2023-05-26 18:47:29 +0000

    Create delete room template and view

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AddRoom, Rooms, RoomDetails
+from .views import AddRoom, Rooms, RoomDetails, DeleteRoom
 
 
 urlpatterns = [
     path('add/', AddRoom.as_view(), name='add_room'),
     path('', Rooms.as_view(), name='rooms'),
-    path("<slug:pk>/", RoomDetails.as_view(), name='room_details')
+    path("<slug:pk>/", RoomDetails.as_view(), name='room_details'),
+    path('delete/<slug:pk>/', DeleteRoom.as_view(), name='delete_room')
 ]
