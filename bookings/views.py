baa33481d05127ef79ff63bc33f82eab479cfeb9
AgaToma <>
2023-06-04 21:27:49 +0000

    Add filter for existing bookings in new booking

--- bookings/views.py
+++ bookings/views.py
@@ -8,7 +8,7 @@ from .forms import BookingForm
 
 class MakeBooking(LoginRequiredMixin, CreateView):
     """
-    View for logged in user to make their booking
+    View for logged in user to make their booking of selected room
     """
     form_class = BookingForm
     template_name = 'bookings/new_booking.html'
@@ -17,3 +17,12 @@ class MakeBooking(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+        room = form.cleaned_data['room']
+        check_in = form.cleaned_data['check_in']
+        check_out = form.cleaned_data['check_out']
+
+        # get existing bookings for the room
+        booking_list = Booking.objects.filter(room=room)
+        # check if there already are bookings on specified dates
+        for booking in booking_list:
+            if booking.check_in > check_in and booking.check_out > 
