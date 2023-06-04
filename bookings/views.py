b1e20800aa094c6489b20d2b8c68b4eb7dfbbaff
AgaToma <>
2023-06-04 20:39:29 +0000

    Create first part of make booking view

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,19 @@
-from django.views.generic import (CreateView, ListView, 
+from django.views.generic import (CreateView, ListView,
                                   DetailView, DeleteView, UpdateView)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from rooms.models import Room
 from .models import Booking
+from .forms import 
+
+
+class MakeBooking(LoginRequiredMixin, CreateView):
+    """
+    View for logged in user to make their booking
+    """
+    form_class = BookingForm
+    template_name = 'bookings/new_booking.html'
+    success_url = '/rooms/'
+    model = Booking
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
