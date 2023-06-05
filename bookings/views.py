6b5b59917aacc8222d462a7498d44cdd096e56ee
AgaToma <>
2023-06-05 11:11:24 +0000

    Finish make booking view

--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import (CreateView, ListView,
                                   DetailView, DeleteView, UpdateView)
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from datetime import date
+from django.contrib import messages
 from rooms.models import Room
 from .models import Booking
 from .forms import BookingForm
@@ -12,7 +14,7 @@ class MakeBooking(LoginRequiredMixin, CreateView):
     """
     form_class = BookingForm
     template_name = 'bookings/new_booking.html'
-    success_url = '/rooms/'
+    success_url = '/bookings/'
     model = Booking
 
     def form_valid(self, form):
@@ -21,8 +23,19 @@ class MakeBooking(LoginRequiredMixin, CreateView):
         check_in = form.cleaned_data['check_in']
         check_out = form.cleaned_data['check_out']
 
-        # get existing bookings for the room
+        # check room availability
         booking_list = Booking.objects.filter(room=room)
-        # check if there already are bookings on specified dates
         for booking in booking_list:
-            if booking.check_in > check_in and booking.check_out > 
+            if booking.check_in.date() > check_out.date() or
+            booking.check_out.date() < check_in.date():
+                form.instance.room = room
+                messages.success(
+                    self.request,
+                    f'Thank you for your booking. Your booking id is {self.pk}')
+        
+        return super(MakeBooking, self).form_valid(form)
+            
+
+        
+        
+
