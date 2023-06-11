38d65dafbc9ff88b3c628780a21c2be59767b15f
AgaToma <>
2023-06-11 17:46:17 +0000

    Booking detail logic cleanup

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,5 +4,5 @@ from .views import MakeBooking, BookingsList, BookingDetails
 urlpatterns = [
     path('newbooking/', MakeBooking.as_view(), name='new_booking'),
     path('', BookingsList.as_view(), name='mybookings'),
-     path("<slug:pk>/", BookingDetails.as_view(), name='booking_details'),
+    path("<slug:pk>/", BookingDetails.as_view(), name='booking_details'),
 ]
