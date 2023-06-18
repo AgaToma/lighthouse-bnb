4a2c63234086cf192f65ac48e6af54f88d5237f2
AgaToma <>
2023-06-18 16:49:14 +0000

    test:booking app setup test user

--- bookings/tests.py
+++ bookings/tests.py
@@ -1,3 +1,25 @@
+from datetime import date
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Booking
+from users.models import CustomUser
+from rooms.models import Room
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    """Tests for booking app views"""
+    def setUp(self):
+        # create test user
+        email = "testuser@test.ie"
+        password = "test123456"
+        user_model = get_user_model()
+        self.user = user_model.objects.create_user(
+            email=email, password=password
+        )
+        logged_in = self.client.login(email=email, password=password)
+        self.assertTrue(logged_in)
+    
+        # create test room
+        self.room = Room.objects.create(
+            
+        )
