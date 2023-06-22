880859fe3d735ae7e70eb3fac22d00212eca5619
AgaToma <>
2023-06-22 12:51:58 +0000

    Add room display on home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from rooms.models import Room
 
 
-class Home(TemplateView):
+class Home(ListView):
     template_name = "home/index.html"
+    model = Room
+    context_object_name = 'rooms'
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+
+
