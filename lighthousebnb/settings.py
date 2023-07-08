7fd05c3779de849802830834b194dfaed561c18f
AgaToma <>
2023-07-08 16:30:26 +0000

    fix: remove files from static url

--- lighthousebnb/settings.py
+++ lighthousebnb/settings.py
@@ -197,7 +197,7 @@ STATIC_URL = '/static/'
 if 'DEVELOPMENT' in os.environ:
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
